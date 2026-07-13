52d5215a05a5268b60a12c3f2fe1d21f306fb26f
neodevgo-prog <neodevgo@gmail.com>
2026-07-13 22:55:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def analizar_texto(texto):
         "total_silabas": total_silabas
     }
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/inicio", methods=["GET", "POST"])
 def home():
     texto_ingresado = ""
     resultados = None
